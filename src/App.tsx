6ff21aa7beace5e51f51bc2539025dcb28e39385
arunn <arunias@gmail.com>
2025-03-10 17:51:34 +0530

    Update logo

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>FlowCoPilot</h1>
+        <div className="logo-container">
+          <img src="/icons/logo.svg" alt="FlowCoPilot Logo" className="app-logo" />
+          <h1>FlowCoPilot</h1>
+        </div>
         <Pomodoro />
       </header>
     </div>
